b35dba49c595962dd7eeda442ca055998681268f
jh11g20 <jh11g20@soton.ac.uk>
2023-10-17 19:25:31 +0100

    Made movement work and added collisions (not physics based), basic ice movement for the game now works

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,40 +7,60 @@ public class Movement : MonoBehaviour
     private bool isMoving;
     [SerializeField] private float gridSize = 1f;
     [SerializeField] private float moveDuration = 0.1f;
+    private Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        System.Func<KeyCode, bool> movement;
-        movement = Input.GetKeyDown;
+        if(!isMoving)
+        {
+            System.Func<KeyCode, bool> movement;
+            movement = Input.GetKey;
 
-        if (movement(KeyCode.UpArrow))
-        {
-            StartCoroutine(Move(Vector2.up));
-        } else if (movement(KeyCode.DownArrow))
-        {
-            StartCoroutine(Move(Vector2.down));
-        } else if (movement(KeyCode.LeftArrow))
-        {
-            StartCoroutine(Move(Vector2.left));
-        } else if (movement(KeyCode.RightArrow))
-        {
-            StartCoroutine(Move(Vector2.right));
+            if (movement(KeyCode.UpArrow))
+            {
+                StartCoroutine(Move(Vector2.up));
+            }
+            else if (movement(KeyCode.DownArrow))
+            {
+                StartCoroutine(Move(Vector2.down));
+            }
+            else if (movement(KeyCode.LeftArrow))
+            {
+                StartCoroutine(Move(Vector2.left));
+            }
+            else if (movement(KeyCode.RightArrow))
+            {
+                StartCoroutine(Move(Vector2.right));
+            }
         }
 
-        
     }
 
     private IEnumerator Move(Vector2 direction)
     {
+        Vector2 adj = direction * 0.5f * gridSize;
+       
+        
         isMoving = true;
 
         Vector2 startPos = transform.position;
+
+        Vector2 hitObj = startPos;
+
+        RaycastHit2D hit = Physics2D.Raycast(startPos, direction, 100f);
+
+        if(hit)
+        {
+            Debug.Log("hit object at " + hit.point);
+            hitObj = hit.point - adj;
+        }
 
         Vector2 endPos = startPos + (direction * gridSize);
 
@@ -50,11 +70,11 @@ public class Movement : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             float percent = elapsedTime / moveDuration;
-            transform.position = Vector2.Lerp(startPos, endPos, percent);
+            transform.position = Vector2.Lerp(startPos, hitObj, percent);
             yield return null;
         }
 
-        transform.position = endPos;
+        transform.position = hitObj;
 
 
         isMoving = false;

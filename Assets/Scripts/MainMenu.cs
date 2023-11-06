d78d7e62bae668fdc74b3eda3cbd7e720669332e
Thomas Holwill (th2g20) <th2g20@soton.ac.uk>
2023-11-06 15:02:26 +0000

    ManuMenu and Tutorial 1 now in game build

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   
+    public void PlayGame()
+    {
+            SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackButton()
+    {
+
+    }
+}

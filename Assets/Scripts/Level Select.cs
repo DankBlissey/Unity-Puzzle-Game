c9c39e4c1ca06f96d5173ec490b026ab470bfc30
Thomas <th2g20@soton.ac.uk>
2023-11-12 19:17:55 +0000

    added UI options to every screen

--- Assets/Scripts/Level Select.cs
+++ Assets/Scripts/Level Select.cs
@@ -5,14 +5,75 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public void level1()
-    {
-        SceneManager.LoadScene(2);
-    }
 
     public void BackButton()
     {
         SceneManager.LoadScene(0);
     }
+
+    public void level1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void level2()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void level3()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void level4()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void level5()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void level6()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void level7()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void level8()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    public void level9()
+    {
+        SceneManager.LoadScene(11);
+    }
+
+    public void level10()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+    public void level11()
+    {
+        SceneManager.LoadScene(13);
+    }
+
+    public void level12()
+    {
+        SceneManager.LoadScene(14);
+    }
+
+    public void level13()
+    {
+        SceneManager.LoadScene(15);
+    }
 }
+
